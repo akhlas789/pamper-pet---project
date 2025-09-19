be04d138ede398889542e7e4405ef1580f9ce118
Akhlas789 <akhlasuddin54321@gmail.com>
2025-09-19 21:52:38 +0600

    main section end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
-export const theme = {
-    extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
+  theme: {
+    extend: {
+      colors: {
+        golden: "#FFDC26", 
+        clifford: "#da373d",
+      },
+
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+      },
+
+      backgroundImage: {
+        
+        "big-cat": "url('./c1-assets/cat-big.png')",
+        "footer-texture": "url('./c1-assets/footer.png')",
+      },
+    },
+  },
+
+  plugins: [],
+};
